0b65cd138c533cb5cb9f732b18a633ce41edc7f8
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-12-13 18:39:45 +0530

    Update with splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:jokeapp/screens/home_page.dart';
-import 'package:jokeapp/screens/splash_page.dart';
+import 'package:flutter_animated_splash/flutter_animated_splash.dart';
 import 'package:provider/provider.dart';
+
+import 'screens/home_page.dart';
 import 'state/joke_state.dart';
 
 void main() {
@@ -22,11 +23,42 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/splash',
-      routes: {
-        '/splash': (context) => const SplashPage(),
-        '/home': (context) => const HomePage(),
-      },
+      home: AnimatedSplash(
+        type: Transition.scale, // Transition effect
+        durationInSeconds: 3, // Splash screen duration
+        navigator: const HomePage(), // Proper navigation
+        curve: Curves.easeInOut, // Animation curve
+        backgroundColor: Colors.white, // Splash screen background color
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/laughing_image.jpg', // Path to your joke-related icon
+              width: 160,
+              height: 160,
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              "Fetch-Jokes",
+              style: TextStyle(
+                fontSize: 44,
+                fontFamily: 'LobsterTwo-Bold',
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              "Get ready to laugh!",
+              style: TextStyle(
+                fontSize: 16,
+                fontFamily: 'LobsterTwo-Bold',
+                color: Colors.grey,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
